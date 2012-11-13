b4791cb0d364772c5550b0cd7813bfb7667f0eaf
aldria30 <aldria30@B1-L0E009892.national.core.bbc.co.uk>
2012-11-13 16:30:19 +0000

    debugging bug fix!

--- js/ahfowdb-core.js
+++ js/ahfowdb-core.js
@@ -40,7 +40,7 @@ ahfow.artistFormProcess = (function(){
     for(var i = 0; i<temp.length; i++) {
         
         $("#"+temp[i]+"-tracks").html($("#"+temp[i]+"-trackto option").length + "/5 selected");
-        if($("select[name="+temp[i]+"-albumvote] option:checked").val()!= '--select--' ){
+        if($("select[name="+temp[i]+"-albumvote] option:checked").attr('value')!= '--select--' ){
             $("#"+temp[i]+"-album").html($("#"+temp[i]+"-albumvote option:checked").val());
         } else {
             $("#"+temp[i]+"-album").html("not selected");
