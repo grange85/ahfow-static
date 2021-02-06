4af926f62f7f17d40b41e1bd26c71c21844a44a8
Andy Aldridge <andy@grange85.co.uk>
2021-02-06 22:43:22 +0000

    update deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+set -euo pipefail
+
+echo "Deploying A Head Full of Wishes database"
+
+source _cloudfront-distribution-id
+
+# upload to s3
+echo "sync content..."
+aws s3 sync --size-only --delete --exclude '.sass-cache'  deploy/ s3://static.fullofwishes.co.uk
+
+# invalidate cloudfront
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
+
+
+echo "A Head Full of Wishes static/legacy content successfully deployed."
